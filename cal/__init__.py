16c7c02797c1c3e355d46dccd47443204f506789
Danial Afzal <dafzal@Danials-MacBook-Pro.local>
2014-02-15 13:19:57 -0800

    init

--- cal/__init__.py
+++ cal/__init__.py
@@ -3,3 +3,12 @@ from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__)
 
+app.config['MONGODB_SETTINGS'] = {
+   
+}
+#connect('project1', host='mongodb://localhost/database_name')
+app.config['SECRET_KEY'] = 'supersecretkey123456'
+db = MongoEngine(app)
+
+if __name__ == '__main__':
+  app.run()
