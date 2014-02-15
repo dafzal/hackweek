8608ba5b24eec625547ddbf67cff14d6b387f5c1
Danial Afzal <dafzal@Danials-MacBook-Pro.local>
2014-02-15 14:10:36 -0800

    add routes

--- cal/manage.py
+++ cal/manage.py
@@ -1,6 +1,7 @@
 import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from cal import app
+from cal import db
 from flask.ext.script import Manager, Server
 
 manager = Manager(app)
@@ -8,6 +9,13 @@ manager = Manager(app)
 manager.add_command('runserver', Server(
   use_debugger = True,
   use_reloader = True))
+
+@manager.command
+def syncdb():
+    from cal.models import User
+    from social.apps.flask_app import models
+    db.drop_all()
+    db.create_all()
 
 if __name__ == '__main__':
   app.config['DEBUG'] = True
