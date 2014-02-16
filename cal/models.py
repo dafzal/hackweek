78ab3e37cd997f116cd0b507562cdb0e3580b635
Danial Afzal <dafzal@Danials-MacBook-Pro.local>
2014-02-15 16:01:49 -0800

    .

--- cal/models.py
+++ cal/models.py
@@ -1,18 +1,22 @@
 from flask.ext.login import UserMixin
 from cal import db
+import facebook
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(200))
-    password = db.Column(db.String(200), default='')
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(200))
-    active = db.Column(db.Boolean, default=True)
+  __tablename__ = 'users'
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(200))
+  password = db.Column(db.String(200), default='')
+  name = db.Column(db.String(100))
+  email = db.Column(db.String(200))
+  active = db.Column(db.Boolean, default=True)
 
-    def is_active(self):
-        return self.active
+  def is_active(self):
+    return self.active
 
-    def facebook_me(self):
-      return self.social_auth.first().extra_data
+  def facebook_me(self):
+    token = self.social_auth.first().extra_data['access_token']
+    graph = facebook.GraphAPI(token)
+    profile = graph.get_object("me")
+    return profile
